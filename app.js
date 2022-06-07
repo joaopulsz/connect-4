cb1a91dcdb40c170da75894a222b62d249ddf91e
Joao Pedro Canale Pulsz <joaopulsz@Joaos-MacBook-Pro.local>
2022-06-07 14:56:35 +0100

    implemented versus mode

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const currentPlayer = document.querySelector('#turn');
+const currentTurn = document.querySelector('#turn');
+let currentPlayer = 1;
 
-const grid = document.querySelector('#grid');
+const columns = document.querySelectorAll('.column');
 
-for (let i = 0; i < 42; i++) {
-    const square = document.createElement('div');
-    grid.append(square);
+columns.forEach((column) => {
+    for (let i = 0; i < 6; i++) {
+        const square = document.createElement('div');
+        column.append(square);
+    };
+
+    column.addEventListener('click', () => {
+        newMove(column);
+    });
+});
+
+function newMove(column) {
+    const squares = column.querySelectorAll('div');
+    const currentMove = Array.from(squares).reverse();
+    const currentSquare = currentMove.find(square => square.style.backgroundColor === '');
+
+    if (currentPlayer === 1) {
+        currentSquare.style.backgroundColor = 'red';
+        currentPlayer = 2;
+    } else if (currentPlayer === 2) {
+        currentSquare.style.backgroundColor = 'yellow';
+        currentPlayer = 1;
+    }
+
+    currentTurn.innerText = currentPlayer;
+
 };
-
