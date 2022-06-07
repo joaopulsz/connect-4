c3075457720dbfb676cd22efbda0463eaa2853c9
Joao Pedro Canale Pulsz <joaopulsz@Joaos-MacBook-Pro.local>
2022-06-07 13:04:46 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const currentPlayer = document.querySelector('#turn');
+
+const grid = document.querySelector('#grid');
+
+for (let i = 0; i < 42; i++) {
+    const square = document.createElement('div');
+    grid.append(square);
+};
+
